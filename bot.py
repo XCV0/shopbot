26ddf0c822be650fda84bc744a55f5c9f0fc82dd
XCV0 <frt.mrartem@gmail.com>
2025-12-05 21:34:58 +0300

    commit

---
+++ bot.py
@@ -0,0 +1,13 @@
+from aiogram import Bot, Dispatcher
+import asyncio
+
+async def main():
+    bot = Bot(token="8404133001:AAHEW9DXaKErO4gD_8rXHSa-XQ13X1Xbu8c")
+    dp = Dispatcher()
+
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
