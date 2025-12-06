5ea78b48c591ac1730ea642de443f318156884f8
XCV0 <frt.mrartem@gmail.com>
2025-12-06 09:29:08 +0300

    miniup

--- handlers/users.py
+++ handlers/users.py
@@ -9,13 +9,14 @@ from aiogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 router = Router()
 
 # ВРЕМЕННО АЙДИ НЕ С БАЗЫ
-available_employees = []
+available_employees = [5201148794, 837411435]
 
 @router.message(Command("start")) 
 async def cmd_start(message: Message):
     if message.from_user.id not in available_employees:
-        await message.answer("Вы не добавлены в пользовтаелей бота!Обратитесь к менеджеру в вашем офисе.\n Ваш ID: " + str(message.from_user.id))
-    
+        await message.answer("Вы не добавлены в пользовтаелей бота!Обратитесь к менеджеру в вашем офисе.\nВаш ID: " + str(message.from_user.id))
+        return
+        
     kb = [[InlineKeyboardButton(
         text="Попробовать", callback_data="create_order"
     )], [InlineKeyboardButton(
